7b8ea5ded098f22c5644cceed5eb0f69bccaa565
Ishankoradia <ikoradia@umich.edu>
2024-05-10 13:16:53 +0530

    reading docx files

--- llm/utils/document.py
+++ llm/utils/document.py
@@ -2,6 +2,7 @@ from typing import List
 from langchain_community.document_loaders.base import BaseLoader
 from langchain_community.document_loaders.text import TextLoader
 from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain.schema import Document
 
@@ -18,6 +19,11 @@ class OpenLLMDocumentLoader(BaseLoader):
             self.loader = PyPDFLoader(file_path)
         elif content_type == "text/markdown":
             self.loader = UnstructuredMarkdownLoader(file_path, mode="elements")
+        elif (
+            content_type
+            == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ):
+            self.loader = Docx2txtLoader(file_path)
         else:
             raise ValueError(f"Unsupported file type: {content_type}")
 
